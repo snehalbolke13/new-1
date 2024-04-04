7d3a61c449ec184a9c9632a708547f1dcfe0a512
a <aemail@gmail.com>
2024-04-05 05:27:40 +0530

    add

---
+++ add1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+	cout<<a+b;
+}
+int main()
+{
+	int x,y,result;
+	cout<<"value of x";
+	cin>>x;
+	cout<<"value of y";
+	cin>>y;
+	add(x,y);
+}
